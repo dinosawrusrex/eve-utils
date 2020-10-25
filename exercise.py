a3dc3870ebcd82bcf9236c00607a29c9e1e9322e
dinosawrusrex <dennissaw12@gmail.com>
2020-10-25 01:59:27 -0700

    Tacked on a couple of bits to migrate Academic stuff

--- exercise.py
+++ exercise.py
@@ -1,9 +1,10 @@
 import os
 import re
+import string
 
 import bs4
 
-CONVERSION = {
+GENERAL_CONVERSION = {
     'Food & Eating': 'food_eat',
     'Transitions': 'transitions',
     'Crime & the Law': 'crime_law',
@@ -24,16 +25,46 @@ CONVERSION = {
     'Phrasal Verbs': 'phrasalverbs'
 }
 
-DIRECTORY = 'gerry_website/general/{}'.format
+ACADEMIC_CONVERSION = {
+    'Sublist 1': 'sublist1',
+    'Sublist 2': 'sublist2',
+    'Sublist 3': 'sublist3',
+    'Sublist 4': 'sublist4',
+    'Sublist 5': 'sublist5',
+    'Sublist 6': 'sublist6',
+    'Sublist 7': 'sublist7',
+    'Sublist 8': 'sublist8',
+    'Sublist 9': 'sublist9',
+    'Sublist 10': 'sublist10',
+}
+
+GENERAL_NAME_FILTER = filter(lambda f: GENERAL_CONVERSION[category] in f.lower(), os.listdir('gerry_website/general'))
+
+def files_filter(category, conversion):
+
+    if 'Sublist' not in category:
+        return filter(lambda f: conversion[category] in f.lower(), os.listdir('gerry_website/general'))
+
+    sublist, _, group = category.partition(' ')
+    if len(group) == 1:
+        group = '0' + group
+    return filter(lambda f: sublist+group in f, os.listdir('gerry_website/academic'))
+
+
+GENERAL_DIRECTORY = 'gerry_website/general/{}'.format
+ACADEMIC_DIRECTORY = 'gerry_website/academic/{}'.format
 
 
 def parse_exercises(category):
 
-    if category not in CONVERSION:
+    if category not in GENERAL_CONVERSION and category not in ACADEMIC_CONVERSION:
         raise ValueError(f'{category} provided incorrect')
     exercises = {}
 
-    for name in filter(lambda f: CONVERSION[category] in f.lower(), os.listdir(DIRECTORY(''))):
+    CONVERSION = GENERAL_CONVERSION if category in GENERAL_CONVERSION else ACADEMIC_CONVERSION
+    DIRECTORY = GENERAL_DIRECTORY if category in GENERAL_CONVERSION else ACADEMIC_DIRECTORY
+
+    for name in sorted(files_filter(category, CONVERSION)):
 
         group, subgroup = _get_group_subgroup(name)
 
@@ -57,6 +88,9 @@ CATEGORY_SUBGROUP = re.compile(r'EngVocEx_\D+_\d+[-_](\d+).htm')
 PHRASAL_VERB_GROUP = re.compile(r'PhrasalVerbsGroup(\d+).*')
 PHRASAL_VERB_SUBGROUP = re.compile(r'PhrasalVerbsGroup\d+-(\d+).htm')
 
+ACADEMIC_GROUP_SUBGROUP = re.compile(r'AWLSublist\d+-Ex(\d)([a-e]).htm')
+ACADEMIC_SUBGROUP_CONVERSION = string.ascii_letters[:5]
+
 
 def _get_group_subgroup(file_name):
 
@@ -72,6 +106,10 @@ def _get_group_subgroup(file_name):
         if not subgroup_match:
             return '1', group_match.group(1)
         return group_match.group(1), subgroup_match.group(1)
+
+    if 'AWLSublist' in file_name:
+        if (match := ACADEMIC_GROUP_SUBGROUP.match(file_name)):
+            return match.group(1), ACADEMIC_SUBGROUP_CONVERSION.index(match.group(2))+1
 
     raise ValueError(f'Could not get name or group for {file_name}')
 
--- words.py
+++ words.py
@@ -7,7 +7,7 @@ import bs4
 GROUP_NUMBER = re.compile(r'Group (\d+).*')
 
 
-CONVERSION = {
+GENERAL_CONVERSION = {
     'All Adjectives': 'alladjectives',
     'Arts, Entertainment, & Literature': 'artsentlit',
     'Crime & the Law': 'crimelaw',
@@ -28,14 +28,31 @@ CONVERSION = {
     'War, Violence, & Conflict': 'warviocon',
 }
 
+ACADEMIC_CONVERSION = {
+    'Sublist 1': 'sublist1',
+    'Sublist 2': 'sublist2',
+    'Sublist 3': 'sublist3',
+    'Sublist 4': 'sublist4',
+    'Sublist 5': 'sublist5',
+    'Sublist 6': 'sublist6',
+    'Sublist 7': 'sublist7',
+    'Sublist 8': 'sublist8',
+    'Sublist 9': 'sublist9',
+    'Sublist 10': 'sublist10',
+}
+
 
 def parse_words(category):
 
-    if category not in CONVERSION:
+    if category not in GENERAL_CONVERSION and category not in ACADEMIC_CONVERSION:
         raise ValueError(f'{category} provided incorrect.')
+
+    CONVERSION = GENERAL_CONVERSION if category in GENERAL_CONVERSION else ACADEMIC_CONVERSION
+    page = 'general' if category in GENERAL_CONVERSION else 'academic'
+
     words = {}
 
-    with open('gerry_website/general/page.html') as f:
+    with open(f'gerry_website/{page}/page.html') as f:
         text = bs4.BeautifulSoup(f.read(), 'html.parser')
 
     category_tag = text.find('h3', id=CONVERSION[category])
